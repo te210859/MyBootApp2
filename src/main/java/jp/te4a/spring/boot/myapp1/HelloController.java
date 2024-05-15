4da06a8245e48197924114f13205e7a83468bc51
牧野 智胤 <210859@jc-21.jp>
2024-05-15 10:43:33 +0900

    課題初回登録

---
+++ src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
@@ -0,0 +1,22 @@
+package jp.te4a.spring.boot.myapp2;
+ 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+/**
+* Hello world!
+*
+*/
+@RestController
+public class HelloController 
+{
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, Spring Boot!";
+	}
+	
+	@RequestMapping("/taro")
+	public String taro() {
+		return "Taro desu!";
+	}
+}
